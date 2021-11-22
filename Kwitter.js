10bf9464c667187b334fd6ba93a9eaaf413391d1
Alek755 <80863873+Alek755@users.noreply.github.com>
2021-11-22 15:53:22 -0500

    Add files via upload

---
+++ Kwitter.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyANWi9612mzbZXQCaf0a380rovRnoMlKqA",
+    authDomain: "chat-223ca.firebaseapp.com",
+    databaseURL: "https://chat-223ca-default-rtdb.firebaseio.com",
+    projectId: "chat-223ca",
+    storageBucket: "chat-223ca.appspot.com",
+    messagingSenderId: "409116211135",
+    appId: "1:409116211135:web:e4a9db56aaaba42d02130a",
+    measurementId: "G-9KRS2PSC3B"
+  };
+  
+  var app = initializeApp(firebaseConfig);
+  var analytics = getAnalytics(app);
